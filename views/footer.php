a47c0a6a8b3113ad38fae53253dfc58b04c96478
Andrew Breakspear <andrew@spy.seh.ox.ac.uk>
2014-07-24 12:28:11 +0100

    Updating foot copyright date

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 <hr />
 <div class="row footer">
 	<div class="col-lg-12 hidden-print">
-		<p class="pull-left">&copy; <a href="#">Andrew Breakspear</a> 2012-2013 <i>Version: <?php echo SITE_VERSION; ?></i></p>
+		<p class="pull-left">&copy; <a href="#">Andrew Breakspear</a> 2012-2014 <i>Version: <?php echo SITE_VERSION; ?></i></p>
 		<p class="pull-right hidden-print">
 			<a href="index.php">Home</a> |
 			<a href="#">Back to top <i class="icon-chevron-up"></i></a>
