2f5949267fc1f0e95219ad0101a558d96db074bb
Andrew Breakspear <andrew@spy.seh.ox.ac.uk>
2014-07-28 11:55:19 +0100

    Updating index information
    Fixes #34

--- nodes/index.php
+++ nodes/index.php
@@ -5,8 +5,8 @@
 		<p><a class="btn btn-primary" href="http://www.ox.ac.uk/admissions/undergraduate/applying-to-oxford/interviews/interview-timetable">View details <i class="fa fa-chevron-right"></i></a></p>
 	</div>
 	<div class="col-lg-6">
-		<h2>Visiting Oxford</h2>
-		<p>Visit Oxfordshire and uncover hidden treasures. From the varied rural landscape of Oxfordshire, excellent for cyclists and walkers, to a cultural getaway in Oxford, renowned for its history and heritage.</p>
-		<p><a class="btn btn-primary" href="http://www.visitoxfordandoxfordshire.com">View details <i class="fa fa-chevron-right"></i></a></p>
+		<h2>Interviews at Oxford</h2>
+		<p>There are lots of myths about interviews at Oxford, but really they're just conversations about your chosen subject - like a short tutorial. We recommend that you give some thought to preparing for your interviews, you may find the advice on the Oxford University website helpful in doing so.</p>
+		<p><a class="btn btn-primary" href="http://www.ox.ac.uk/admissions/undergraduate/applying-to-oxford/interviews">View details <i class="fa fa-chevron-right"></i></a></p>
 	</div>
 </div>
