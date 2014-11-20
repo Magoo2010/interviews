1fbd5f3924c5989f76d0fd2ce80664a347e04253
Andrew Breakspear <andrew@longhanborough.com>
2014-11-20 14:41:55 +0000

    Deleting records now fixed
    Wrong path for remote action call when deleting student.  Fixes #43

--- modules/students/actions/delete.php
+++ modules/students/actions/delete.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . 'engine/initialise.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/engine/initialise.php');
 $user = Students::find_by_uid($_POST['uid']);
 
 if ($directory = opendir(SITE_LOCATION . "/uploads/")) {
